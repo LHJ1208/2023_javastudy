ce6ee630d6921d7ee1fcea571870066ddc383a35
Hanju <leehanju1208@gmail.com>
2023-05-23 17:24:55 +0900

    Hello Hello Hello Git

--- src/day20/com/ict/edu3/Ex01_Menu.java
+++ src/day20/com/ict/edu3/Ex01_Menu.java
@@ -2,7 +2,14 @@ package day20.com.ict.edu3;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.util.Enumeration;
+import java.util.Iterator;
 
+import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -12,7 +19,7 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
-public class Ex01_Menu extends JFrame {
+public class Ex01_Menu extends JFrame implements ActionListener, ItemListener {
 	JPanel jp1, jp2, jp3, jp4, jp5;
 
 	JRadioButton jrb1, jrb2, jrb3, jrb4, jrb5, jrb6, jrb7, jrb8, jrb9, jrb10;
@@ -36,7 +43,7 @@ public class Ex01_Menu extends JFrame {
 
 	int[] pricePizza = { PRICE_COMBO, PRICE_POTATO, PRICE_BULGOGI };
 	int[] priceTopping = { PRICE_PARPRIKA, PRICE_CHEESE, PRICE_PEPPERONI, PRICE_BACON };
-	double[] multiplyRateSize = { MULTIPLY_RATE_SMALL, MULTIPLY_RATE_MEDIUM, MULTIPLY_RATE_LARGE };
+	double[] priceMultiplySize = { MULTIPLY_RATE_SMALL, MULTIPLY_RATE_MEDIUM, MULTIPLY_RATE_LARGE };
 
 	double price = (PRICE_COMBO + PRICE_PARPRIKA) * MULTIPLY_RATE_MEDIUM; // 초기값
 
@@ -90,8 +97,8 @@ public class Ex01_Menu extends JFrame {
 
 		bg3 = new ButtonGroup();
 
-		jrb8 = new JRadioButton("Small(80%)", true);
-		jrb9 = new JRadioButton("Medium(100%)");
+		jrb8 = new JRadioButton("Small(80%)");
+		jrb9 = new JRadioButton("Medium(100%)", true);
 		jrb10 = new JRadioButton("Large(120%)");
 
 		bg3.add(jrb8);
@@ -125,6 +132,104 @@ public class Ex01_Menu extends JFrame {
 
 		setResizable(false);
 
+		jb1.addActionListener(this);
+		jb2.addActionListener(this);
+
+		jrb1.addItemListener(this);
+		jrb2.addItemListener(this);
+		jrb3.addItemListener(this);
+		jrb4.addItemListener(this);
+		jrb5.addItemListener(this);
+		jrb6.addItemListener(this);
+		jrb7.addItemListener(this);
+		jrb8.addItemListener(this);
+		jrb9.addItemListener(this);
+		jrb10.addItemListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton obj = (JButton) e.getSource();
+
+		if (obj == jb1) {
+
+//			price = 0;
+//
+//			if (jrb1.isSelected())
+//				price += PRICE_COMBO;
+//			else if (jrb2.isSelected())
+//				price += PRICE_POTATO;
+//			else if (jrb3.isSelected())
+//				price += PRICE_BULGOGI;
+//
+//			if (jrb4.isSelected())
+//				price += PRICE_PARPRIKA;
+//			else if (jrb5.isSelected())
+//				price += PRICE_CHEESE;
+//			else if (jrb6.isSelected())
+//				price += PRICE_PEPPERONI;
+//			else if (jrb7.isSelected())
+//				price += PRICE_BACON;
+//
+//			if (jrb8.isSelected())
+//				price *= MULTIPLY_RATE_SMALL;
+//			else if (jrb9.isSelected())
+//				price *= MULTIPLY_RATE_MEDIUM;
+//			else if (jrb10.isSelected())
+//				price *= MULTIPLY_RATE_LARGE;
+
+			jtf.setText((int) price + " 원");
+		} else if (obj == jb2) {
+			jrb1.setSelected(true);
+			jrb4.setSelected(true);
+			jrb9.setSelected(true);
+			price = (PRICE_COMBO + PRICE_PARPRIKA) * MULTIPLY_RATE_MEDIUM;
+			jtf.setText("");
+		}
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent e) {
+		// bg1에 존재하는 요소들을 배열(?)로 만든다.
+		Enumeration<AbstractButton> list1 = bg1.getElements();
+
+		int i = 0;
+		while (list1.hasMoreElements()) {
+			// bg1의 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) list1.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				price = pricePizza[i];
+				break;
+			}
+			i++;
+		}
+
+		Enumeration<AbstractButton> list2 = bg2.getElements();
+		i = 0;
+		while (list2.hasMoreElements()) {
+			// bg2의 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) list2.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				price += priceTopping[i];
+				break;
+			}
+			i++;
+		}
+
+		Enumeration<AbstractButton> list3 = bg3.getElements();
+		i = 0;
+		while (list3.hasMoreElements()) {
+			// bg3의 요소를 꺼내서 JRadioButton으로 형변환
+			AbstractButton abstractButton = (AbstractButton) list3.nextElement();
+			JRadioButton jb = (JRadioButton) abstractButton;
+			if (jb.isSelected()) {
+				price *= priceMultiplySize[i];
+				break;
+			}
+			i++;
+		}
 	}
 
 	public static void main(String[] args) {
